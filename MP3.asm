c8318df252f293957c204f11658a12b2964ad29a
joecatarata <joecatarata@gmail.com>
2017-07-29 18:33:27 +0800

    Added some variables
    Scanning is not working right

--- MP3.asm
+++ MP3.asm
@@ -1,8 +1,34 @@
 %include "io.inc"
+extern _system, _printf, _scanf, _getchar, _puts
+
+section .data
+clrscr db "cls",0
+
+
+formatFloat db "%g", 0
+eulerMessage db "e^x = ", 0
+approx db "Approximations: ", 0
+inputMsg db "Input a number: ", 0
+number dd 0.0
 
 section .text
 global _main
 _main:
-    ;write your code here
+    mov ebp, esp
+    
+    push inputMsg
+    call _printf
+    add esp , 4
+    
+    
+    push number
+    push formatFloat 
+    call _scanf
+    add esp, 8
+    
+    push formatFloat 
+    call _printf 
+    add esp, 4
+    
     xor eax, eax
     ret
