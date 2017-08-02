d95575803080434b17b8df1e3201a2809fe6d6a8
joecatarata <joecatarata@gmail.com>
2017-08-03 00:30:52 +0800

    Fixed scanning

--- MP3.asm
+++ MP3.asm
@@ -5,30 +5,43 @@ section .data
 clrscr db "cls",0
 
 
-formatFloat db "%g", 0
+formatFloat db "%lg", 0
+formatOut db "Input is %lg  ",0
+term db "Term #%d", 13,10,0
+counter db 0
+termNum db 0
 eulerMessage db "e^x = ", 0
 approx db "Approximations: ", 0
 inputMsg db "Input a number: ", 0
-number dd 0.0
+testCtr db "Ctr is %d", 0
+number dq 0.0
 
 section .text
 global _main
 _main:
     mov ebp, esp
+    emms
     
+
     push inputMsg
     call _printf
-    add esp , 4
+    add esp , 0x4
     
     
     push number
     push formatFloat 
     call _scanf
-    add esp, 8
+    add esp, 0x8
     
-    push formatFloat 
+    push dword[number+4]
+    push dword[number]
+    push formatOut 
     call _printf 
-    add esp, 4
+    add esp, 0xC
     
-    xor eax, eax
+    ;make loop for Taylor Series    
+    mov ecx, 0x14   
+    mov dword[counter], 0x0 ;Initialize Counter   
+    mov dword[termNum], 0x1 ;Initialize Term Number
+    
     ret
