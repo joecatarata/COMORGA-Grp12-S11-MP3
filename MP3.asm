2d746bcb816798b3fbef8947513914c53698cb62
joecatarata <joecatarata@gmail.com>
2017-08-10 23:49:34 +0800

    Added exponent
    Working exponent

--- MP3.asm
+++ MP3.asm
@@ -1,23 +1,37 @@
-%include "io.inc"
+
 extern _system, _printf, _scanf, _getchar, _puts
+
+Zero:
+
+    
+    jmp reset
+
+ZeroFactorial:
+
+
 
 section .data
 clrscr db "cls",0
 
 
-formatFloat db "%lg", 0
-formatOut db "Input is %lg  ",0
+formatFloat db "%lf", 0
+formatOut db "Input is %lf  ",0
 term db "Term #%d: ", 13,10,0
-expCounter db 0
-termNum db 0
+expCounter dd 0
+termNum dd 0
 eulerMessage db "e^x = ", 0
 approx db "Approximations: ", 0
 inputMsg db "Input a number: ", 0
 testCtr db "Ctr is %d", 0
+testString db "%lg",13,10, 0
 temp dd 0
+tempEx dd 0
 orig dd 0
-number dq 0.0
+total dq 0.0
+inputFloat dq 0.0
 factorialDivisor dd 0.0
+originalInput dq 0.0
+exponentAnswer dq 0.0
 ; try manipulation equation: x^n
 ; solved new expression: x^n = 2^(n*log2(x))
 section .text
@@ -32,36 +46,52 @@ _main:
     add esp , 0x4
 
 
-    push number
+    push inputFloat
     push formatFloat
     call _scanf
     add esp, 0x8
 
-    push dword[number+4]
-    push dword[number]
+    push dword[inputFloat+4]
+    push dword[inputFloat]
     push formatOut
     call _printf
     add esp, 0xC
 
     ;make loop for Taylor Series
     mov ecx, 0x14
-    mov dword[expCounter], 0x0 ;Initialize Counter
+    mov dword[expCounter], 0x20 ;Initialize Counter
     mov dword[termNum], 0x1 ;Initialize Term Number
     
+    fld qword[inputFloat]
+    fst qword[originalInput]
+    emms
+    
 Taylor:
+    fld qword[originalInput]
+    fst qword[inputFloat]
+    emms
+    
     mov dword[orig], ecx
-    mov ecx, dword[expCounter]
+    mov esi, dword[expCounter]
     ;x^n
     Exponent:
-      cmp ecx, 0x0
+      cmp esi, 0x0
       jz reset
       
-      fld qword[number]
-      fld qword[number]
-      fmul st0, st1
-      fst qword[number]
+      fld qword[originalInput]
+      fld qword[inputFloat]
+      fmul st0,st1
+      fst qword[inputFloat]
       emms
       
+      push dword[inputFloat+4]
+      push dword[inputFloat]
+      push testString
+      call _printf
+      add esp, 0xC
+
+
+      sub esi, 1
       jmp Exponent
 
 
@@ -70,6 +100,7 @@ Taylor:
         
     ;n!
     Factorial:
+        
         
     Divide:
     ;div
@@ -90,7 +121,7 @@ Taylor:
     mov ecx, dword[temp]
     inc dword[termNum]
     
-    loop Taylor
+    ;loop Taylor
     
     xor eax, eax
     ret
